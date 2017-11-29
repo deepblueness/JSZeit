9f2168fbf7c58782967c6c3c1fabd2b4bfeed2a5
Anastasia Chicu <anastasia.chicu@xing.com>
2017-11-29 15:08:57 +0100

    after lint

--- 5-Advanced-JS/question.js
+++ 5-Advanced-JS/question.js
@@ -1,61 +1,55 @@
-var Question  = function (text, answerOptions, correctAnswer) 
-{
+var Question = function (text, answerOptions, correctAnswer) {
     this.text = text;
     this.answerOptions = answerOptions;
-    this.correctAnswer  = correctAnswer;
-    
+    this.correctAnswer = correctAnswer;
+
 }
 var counter = 0;
 
 Question.prototype.getRandomQuestion = function () {
-   
-  var chosenOne= availableQuestions[ Math.floor(Math.random()*4)];
-  console.log(chosenOne.text +'\n'); 
-  chosenOne.answerOptions.forEach(function(option) {
-    console.log(chosenOne.answerOptions.indexOf(option)+': '+ option +'\n');
-  });
-  Question.prototype.verifyAnswer = function(){
-    var answer = prompt('Give an answer', ' ');
-    
-    var count = function (ans) {
-        if (ans) {
-        counter ++; 
-        console.log ('your score is '+ counter)}
-        else  console.log ('your score is '+ counter)
-    }
-   
 
-    if (answer == chosenOne.correctAnswer) {
-       
-        count(true);
-        console.log('correct answer');  
-        
-        this.getRandomQuestion();
-        
-        return this.verifyAnswer();
-    }
-    else if (answer == 'exit') {return }
-    else {console.log('wrong answer'); {
-        this.getRandomQuestion();
-        count(false)
-        return this.verifyAnswer();
-    }
-    
- }     
-}
-  
-}
-var question1 = new Question 
-("Are you cool?", ['yes', 'no'], 0);
-var question2 = new Question 
-("Is JS amazing?", ['yes', 'no'], 0);
-var question3 = new Question 
-("Are you the best programmer ever?", ['yes', 'no'], 0);
-var question4 = new Question 
-("Where is Moldova located?", ['in Africa', 'in Latin America', 'in Europe'], 2);
+    var chosenOne = availableQuestions[Math.floor(Math.random() * 4)];
+    console.log(chosenOne.text + '\n');
+    chosenOne.answerOptions.forEach(function (option) {
+        console.log(chosenOne.answerOptions.indexOf(option) + ': ' + option + '\n');
+    });
+    Question.prototype.verifyAnswer = function () {
+        var answer = prompt('Give an answer', ' ');
 
-var availableQuestions  = [ question1, question2, question3, question4 ];
+        var count = function (ans) {
+            if (ans) {
+                counter++;
+                console.log('your score is ' + counter)
+            } else console.log('your score is ' + counter)
+        }
+
+
+        if (answer == chosenOne.correctAnswer) {
+
+            count(true);
+            console.log('correct answer');
+
+            this.getRandomQuestion();
+
+            return this.verifyAnswer();
+        } else if (answer == 'exit') {
+            return
+        } else {
+            console.log('wrong answer'); {
+                this.getRandomQuestion();
+                count(false)
+                return this.verifyAnswer();
+            }
+
+        }
+    }
+
+}
+var question1 = new Question("Are you cool?", ['yes', 'no'], 0);
+var question2 = new Question("Is JS amazing?", ['yes', 'no'], 0);
+var question3 = new Question("Are you the best programmer ever?", ['yes', 'no'], 0);
+var question4 = new Question("Where is Moldova located?", ['in Africa', 'in Latin America', 'in Europe'], 2);
+
+var availableQuestions = [question1, question2, question3, question4];
 question1.getRandomQuestion();
 question1.verifyAnswer();
-
-
